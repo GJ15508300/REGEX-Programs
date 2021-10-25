81fd96aa80cf0a0c9a7968ac7bd57071840fec4d
GJ15508300 <Jagadeesh15508300@gmail.com>
2021-10-25 11:04:55 -0700

    mailcheck error updated

--- colorcheck.js
+++ colorcheck.js
@@ -1,14 +1,15 @@
 var readlineSync = require('readline-sync')
-console.log("What are the colours of the French flag (in any order)");
-console.log("it's blue, white and red");
-var regex =  "(^(?!.*(blue).*)|^(?!.*(white).*)|^(?!.*(red).*))";
+console.log("What are the colours of the Indian flag (in any order)");
+console.log("it's Orange, white, green");
+var regex = new RegExp ( /^(?!.*(Orange).*)|^(?!.*(white).*)|^(?!.*(green).*)$/ );
 var color = readlineSync.question('enter flag colors');
-var tst = regex.match(color);
+var tst = regex.test(color);
 console.log(tst);
 if(tst==true)
 {
     console.log("VALID");
 }
-else{
+else
+{
     console.log("INVALID");
 }
--- mailcheck.js
+++ mailcheck.js
@@ -1,14 +1,16 @@
 var readlineSync = require('readline-sync')
 var mail = readlineSync.question('enter password');
-var pattern = "^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$";       
-console.log(mail.match(pattern)); 
-//console.log(pattern.test(mail));
-var check= pattern.match(mail);  
-//check= pattern.test(mail);
+var pattern = new RegExp (/^[a-zA-Z][a-zA-Z0-9-_.?]+@[a-zA-Z]*.[a-zA-Z]{2,3}.[a-zA-Z]{2,3}$/);
+var check;     
+//console.log(mail.match(pattern)); 
+console.log(pattern.test(mail));
+// check= pattern.match(mail);  
+check=pattern.test(mail)
 if(check==true)
 {
     console.log("VALID ");
 }
-else{
+else
+{
     console.log("INVALID");
 }
